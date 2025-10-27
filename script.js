e7b9dc8835e9b87c3a7f8b9641b79c70dce04d71
DiegoNotreve <notreveogeide@gmail.com>
2025-10-26 21:36:02 -0300

    projeto em andamento

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirDialog(src) {
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modalImg');
+  modalImg.src = src;
+  modal.showModal();
+}
+
+function fecharDialog() {
+  document.getElementById('modal').close();
+}
